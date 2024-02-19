9895ad893a42306c4e3d5b917c1c45df5b6926a2
NotAGriffon <chrisbruinpacheco@gmail.com>
2024-02-19 11:52:36 -0500

    Unity Update 2/15

--- Assets/Scripts/JsonSerializer.cs
+++ Assets/Scripts/JsonSerializer.cs
@@ -1,56 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 using System.IO;
 using TMPro;
-
+using UnityEngine.Networking;
 public class JsonSerializer : MonoBehaviour
 {
-    // Variables
     public DataClass dataObj;
     public string filePath;
     public TMP_InputField playerName, level, elapsedTime;
 
-    // Start
+    // Start is called before the first frame update
     void Start()
     {
         filePath = Path.Combine(Application.dataPath, "saveData.txt");
-        dataObj = new DataClass();
-        dataObj.level = 1;
-        dataObj.timeElapsed = 212f;
-        dataObj.name = "Christopher";
-
+        //dataObj = new DataClass();
+        //dataObj.level = 1;
+        //dataObj.timeElapsed = 44443.232323f;
+        //dataObj.name = "Jordan";
         string json = JsonUtility.ToJson(dataObj);
-
-        StartCoroutine(SendWebData(json));
-
+        Debug.Log(json);
+        //StartCoroutine(SendWebData(json));
         File.WriteAllText(filePath, json);
     }
 
-    // Update
+    // Update is called once per frame
     void Update()
     {
 
     }
 
-    // Send Button
     public void SendButton()
     {
-        // Local Variables
         var levelData = int.Parse(level.text);
         var timeData = float.Parse(elapsedTime.text);
-
         DataClass formData = new DataClass();
         formData.level = levelData;
         formData.timeElapsed = timeData;
         formData.name = playerName.text;
-
         string json = JsonUtility.ToJson(formData);
         filePath = Path.Combine(Application.dataPath, "formData.txt");
         File.WriteAllText(filePath, json);
-
         StartCoroutine(SendWebData(json));
+
     }
 
     public void GetButton()
@@ -58,30 +50,29 @@ public class JsonSerializer : MonoBehaviour
         StartCoroutine(GetRequest("http://localhost:3000/getUnity"));
     }
 
-    // Send Web Data
     IEnumerator SendWebData(string json)
     {
         using (UnityWebRequest request = UnityWebRequest.Post("http://localhost:3000/unity", json, "application-json"))
         {
             request.SetRequestHeader("content-type", "application/json");
+
             request.uploadHandler.contentType = "application/json";
             request.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(json));
 
             yield return request.SendWebRequest();
 
-            if(request.result != UnityWebRequest.Result.Success)
+            if (request.result != UnityWebRequest.Result.Success)
             {
                 Debug.Log(request.error);
             }
             else
             {
-                Debug.Log("Data Passed");
+                Debug.Log("Data Posted");
             }
             request.uploadHandler.Dispose();
         }
     }
 
-    // Get Request
     IEnumerator GetRequest(string uri)
     {
         using (UnityWebRequest getRequest = UnityWebRequest.Get(uri))
@@ -91,10 +82,24 @@ public class JsonSerializer : MonoBehaviour
             var newData = System.Text.Encoding.UTF8.GetString(getRequest.downloadHandler.data);
             Debug.Log(newData);
             var newGetRequestData = JsonUtility.FromJson<DataClass>(newData);
+            Debug.Log(newGetRequestData);
+            Debug.Log(newGetRequestData);
 
-            Debug.Log(newGetRequestData.name);
-            Debug.Log(newGetRequestData.level);
-            Debug.Log(newGetRequestData.timeElapsed);
+            var newGetRoot = JsonUtility.FromJson<Root>(newData);
+            Debug.Log(newGetRoot.playerdata[0].level);
+
+            for (int i = 0; i < newGetRoot.playerdata.Length; i++)
+            {
+                Debug.Log(newGetRoot.playerdata[i].name);
+                Debug.Log(newGetRoot.playerdata[i].level);
+                Debug.Log(newGetRoot.playerdata[i].timeElapsed);
+
+            }
+
+
+            //Debug.Log(newGetRequestData.name);
+            //Debug.Log(newGetRequestData.level);
+            //Debug.Log(newGetRequestData.timeElapsed);
         }
     }
 }
